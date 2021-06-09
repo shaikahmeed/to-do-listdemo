b8b789629bc2c2a5d9cbd35fc4b739c406a2515a
shaikahmeed <shaikahmeed9@gmail.com>
2021-06-09 06:38:30 +0530

    ..

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todo-app';
+
+  nextId = 3;
+  todos = [
+    {
+      id: 1,
+      label: 'Bring Milk',
+      done: false
+    },
+    {
+      id: 2,
+      label: 'Pay mobile bill',
+      done: false
+    },
+  ];
+
+  delete(todo) {
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+  }
+
+  add(newTodoLabel) {
+    let newTodo = {
+      id: this.nextId,
+      label: newTodoLabel,
+      done: false
+    };
+    this.todos.push(newTodo);
+    this.nextId++;
+  }
+
+  toggle(todo) {
+    todo.done = !todo.done;
+  }
+}
